36c5025e0e1619cb786c3bbe1c057202219d318c
Vedant Purohit <vedant@sportomic.com>
2026-09-01 14:38:50 +0530

    feat: implement organization waiters

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -12,13 +12,14 @@ import type * as menu from "../menu.js";
 import type * as organizationFeatures from "../organizationFeatures.js";
 import type * as organizationLanguages from "../organizationLanguages.js";
 import type * as organizationLayouts from "../organizationLayouts.js";
-import type * as organizationPrinters from "../organizationPrinters.js";
-import type * as organizationTables from "../organizationTables.js";
 import type * as organizationOrderProcesses from "../organizationOrderProcesses.js";
+import type * as organizationPrinters from "../organizationPrinters.js";
 import type * as organizationQrCodes from "../organizationQrCodes.js";
 import type * as organizationQueueConfigurations from "../organizationQueueConfigurations.js";
 import type * as organizationQueues from "../organizationQueues.js";
+import type * as organizationTables from "../organizationTables.js";
 import type * as organizationUsers from "../organizationUsers.js";
+import type * as organizationWaiters from "../organizationWaiters.js";
 import type * as organizations from "../organizations.js";
 import type * as taxation from "../taxation.js";
 
@@ -33,19 +34,20 @@ declare const fullApi: ApiFromModules<{
   organizationFeatures: typeof organizationFeatures;
   organizationLanguages: typeof organizationLanguages;
   organizationLayouts: typeof organizationLayouts;
-  organizationPrinters: typeof organizationPrinters;
-  organizationTables: typeof organizationTables;
   organizationOrderProcesses: typeof organizationOrderProcesses;
+  organizationPrinters: typeof organizationPrinters;
   organizationQrCodes: typeof organizationQrCodes;
   organizationQueueConfigurations: typeof organizationQueueConfigurations;
   organizationQueues: typeof organizationQueues;
+  organizationTables: typeof organizationTables;
   organizationUsers: typeof organizationUsers;
+  organizationWaiters: typeof organizationWaiters;
   organizations: typeof organizations;
   taxation: typeof taxation;
 }>;
 
 /**
- * A utility for referencing Convex functions in your app's public API.
+ * A utility for referencing Convex functions in your app's API.
  *
  * Usage:
  * ```js
@@ -56,18 +58,7 @@ export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
-
-/**
- * A utility for referencing Convex functions in your app's internal API.
- *
- * Usage:
- * ```js
- * const myFunctionReference = internal.myModule.myFunction;
- * ```
- */
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
-
-export declare const components: {};
