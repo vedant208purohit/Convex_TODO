f61abb804f1fde1ae818f68eb7dc3118f132de1a
Vedant Purohit <vedant@sportomic.com>
2026-09-01 14:42:53 +0530

    chore: update generated api types

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -47,7 +47,7 @@ declare const fullApi: ApiFromModules<{
 }>;
 
 /**
- * A utility for referencing Convex functions in your app's API.
+ * A utility for referencing Convex functions in your app's public API.
  *
  * Usage:
  * ```js
@@ -58,7 +58,18 @@ export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {};
